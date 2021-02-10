63d8b3d52b80c0cc9c9148da789c86f18a04cdf8
skoetsier <stefani.koetsier@ordina.nl>
2021-02-10 16:10:27 +0100

    Add song_rater app to the project
    I added the song rater app to the settings file and included
    the song rater url configuration to the upper level url
    configuration

--- song_rater/urls.py
+++ song_rater/urls.py
@@ -1,8 +1,11 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 app_name = 'song_rater'
 
 urlpatterns = [
     path('home', views.home, name='home'),
+    path('add-song', views.add_song, name='add-song'),
+    path('song_added', views.thanx, name='song-added'),
+    path('save-song', views.save_song, name='save-song')
 ]
