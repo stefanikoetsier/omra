1ff0af3961d7a2e2faa17b8046ca8b691f5e724f
skoetsier <stefani.koetsier@ordina.nl>
2021-02-10 11:27:51 +0100

    Add urls and configuration of the song_rater app

--- omra/settings.py
+++ omra/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'song_rater.apps.SongRaterConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
