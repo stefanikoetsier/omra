8b844405ca0268880e250025f0a14fb6c2e6375d
skoetsier <stefani.koetsier@ordina.nl>
2021-02-10 16:12:42 +0100

    Add song model to song rater app
    I added the Model for a song with the attributes 'artist'
    and 'title'

--- song_rater/models/song.py
+++ song_rater/models/song.py
@@ -1,5 +1,6 @@
 from django.db import models
 
-# Create your models here.
-class Song(object):
-    pass
+
+class Song(models.Model):
+    title = models.CharField(max_length=100)
+    artist = models.CharField(max_length=100)
