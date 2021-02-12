9d82196e5f50aad88413798200db705a076a2dbe
skoetsier <stefani.koetsier@ordina.nl>
2021-02-12 12:41:11 +0100

    Add unit test for add song functionality
    I added a test to test strong and retrieving database entries of
    the Song model. Additionally I added a test which tests the presence
    of the add-song form.

--- song_rater/tests.py
+++ song_rater/tests.py
@@ -1,4 +1,5 @@
 from django.test import TestCase, Client
+from .models import Song
 
 
 class HomeTestCase(TestCase):
@@ -7,3 +8,19 @@ class HomeTestCase(TestCase):
         response = c.get('/home')
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, 'Welcome to the homepage of Ordina Music Rater')
+
+
+class AddSongTestCase(TestCase):
+    def setUp(self):
+        Song.objects.create(artist='Apple', title='pie')
+
+    def test_add_song(self):
+        song = Song.objects.get(artist='Apple')
+        self.assertEqual(song.title, 'pie')
+
+    def test_add_song_form(self):
+        c = Client()
+        response = c.get('/add-song')
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, '<form action="/add-song" method="post">')
+        self.assertContains(response, '<input type="submit" value="Submit">')
