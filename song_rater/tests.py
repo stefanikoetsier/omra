b685de480b96b9f92053007beaa5a0ae5a633353
skoetsier <stefani.koetsier@ordina.nl>
2021-02-16 12:59:37 +0100

    Refactored test and added more tests.
    Added test to check imcomplete form, and a test which checks if
    a post for data is correctly stored in the database.

--- song_rater/tests.py
+++ song_rater/tests.py
@@ -4,23 +4,29 @@ from .models import Song
 
 class HomeTestCase(TestCase):
     def test_home_page(self):
-        c = Client()
-        response = c.get('/home')
+        #c = Client()
+        response = self.client.get('/home')
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, 'Welcome to the homepage of Ordina Music Rater')
 
 
 class AddSongTestCase(TestCase):
     def setUp(self):
-        Song.objects.create(artist='Apple', title='pie')
+        Song.objects.create(artist='Apple', title='Pie')
 
     def test_add_song(self):
         song = Song.objects.get(artist='Apple')
-        self.assertEqual(song.title, 'pie')
+        self.assertEqual(song.title, 'Pie')
 
     def test_add_song_form(self):
-        c = Client()
-        response = c.get('/add-song')
+        response = self.client.get('/add-song')
         self.assertEqual(response.status_code, 200)
-        self.assertContains(response, '<form action="/add-song" method="post">')
-        self.assertContains(response, '<input type="submit" value="Submit">')
+
+    def test_add_song_post(self):
+        self.client.post('/add-song', {'artist': 'Strawberry', 'title': 'Shortcake'})
+        song = Song.objects.get(artist='Strawberry')
+        self.assertEqual(song.title, 'Shortcake')
+
+    def test_add_song_incomplete_post(self):
+        response = self.client.post('/add-song', {'artist': 'Orange'})
+        self.assertEqual(response.status_code, 200)
