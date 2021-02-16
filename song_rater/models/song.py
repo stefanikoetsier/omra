b21078ef3ff4670f5de33963577725d5a6121d74
skoetsier <stefani.koetsier@ordina.nl>
2021-02-16 16:01:55 +0100

    Add Model for rating
    I have added a Model for the rating. Since the value is between 0 and 5
    I made use of a positive integer field, and a validator to make sure
    it will never be greater than 5

--- song_rater/models/song.py
+++ song_rater/models/song.py
@@ -1,6 +1,12 @@
 from django.db import models
+from django.core.validators import MaxValueValidator
 
 
 class Song(models.Model):
     title = models.CharField(max_length=100)
     artist = models.CharField(max_length=100)
+
+
+class Rating(models.Model):
+    song = models.ForeignKey(Song, on_delete=models.CASCADE)
+    rating = models.PositiveSmallIntegerField(validators=[MaxValueValidator(5)])
