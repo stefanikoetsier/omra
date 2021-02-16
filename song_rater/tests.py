161da1e0956929a3937aa02d92a90b28e7e56ca8
skoetsier <stefani.koetsier@ordina.nl>
2021-02-16 14:29:07 +0100

    Removed obsolete Client object declaration.
    Now i make use of the self.client instead of c = Client for more readible
    code.

--- song_rater/tests.py
+++ song_rater/tests.py
@@ -1,10 +1,9 @@
-from django.test import TestCase, Client
+from django.test import TestCase
 from .models import Song
 
 
 class HomeTestCase(TestCase):
     def test_home_page(self):
-        #c = Client()
         response = self.client.get('/home')
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, 'Welcome to the homepage of Ordina Music Rater')
