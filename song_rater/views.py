d707a69bf0ea11cd0e3d75770b05c10dc35af1c4
skoetsier <stefani.koetsier@ordina.nl>
2021-02-17 17:23:25 +0100

    Add Rating data to song-list page
    I override get_context_data to add in the Rating data. A new
    list of dicts is added to the context which includes song data,
    and rating data

--- song_rater/views.py
+++ song_rater/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.views.generic.edit import CreateView
 from django.views.generic.list import ListView
-from .models import Song
+from django.db.models import Avg
+from .models import Song, Rating
 
 
 def home(request):
@@ -16,6 +17,27 @@ class SongCreate(CreateView):
 
 class SongList(ListView):
     model = Song
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        song_table = []
+
+        for song in Song.objects.all():
+            ratings = Rating.objects.filter(song=song)
+            n_ratings = ratings.count()
+            avg_rating = ratings.aggregate(Avg('rating'))['rating__avg']
+
+            song_table.append({
+                'artist': song.artist,
+                'title': song.title,
+                'avg_rating': avg_rating,
+                'n_ratings': n_ratings,
+            })
+
+        context['song_table'] = song_table
+
+        return context
 
 
 def song_added(request):
