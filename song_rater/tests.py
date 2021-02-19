1cedbf4c3bf8859cf34f44642a3301bca7af3ed3
skoetsier <stefani.koetsier@ordina.nl>
2021-02-19 16:57:40 +0100

    Add more tests for SongList view
    A helper function is added to add a song and
    a rating to the database. Now I also test the reponse
    of an empty songlist, and a songlist with two songs.
    Also added a comment for clarification.

--- song_rater/tests.py
+++ song_rater/tests.py
@@ -36,13 +36,22 @@ class AddSongTestCase(TestCase):
 
 
 class SongListTestCase(TestCase):
-    def setUp(self):
-        song = Song.objects.create(artist='Apple', title='Pie')
-        song.ratings.create(rating=3)
+    def add_song_and_rating(self, artist, title, rating):  # Helper function
+        song = Song.objects.create(artist=artist, title=title)
+        song.ratings.create(rating=rating)
 
-    def test_song_list(self):
+    def test_song_list_empty(self):
         response = self.client.get('/song-list')
-        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'No songs available yet. Try adding a new song')
+
+    def test_song_list_one_song(self):
+        self.add_song_and_rating(artist='Apple', title='Pie', rating=3)
+
+        response = self.client.get('/song-list')
         self.assertContains(response, 'Apple', count=1)
         self.assertContains(response, 'Pie', count=1)
-        self.assertContains(response, '3', count=1)
+        self.assertContains(response, '3', count=1)  # See if the set average rating is included
+
+    def test_song_list_multiple_songs(self):
+        self.add_song_and_rating(artist='Strawberry', title='Shortcake', rating=4)
+        self.test_song_list_one_song()
