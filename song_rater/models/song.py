59b238f185989ade2a0732a646914b1b5ce4d601
skoetsier <stefani.koetsier@ordina.nl>
2021-02-24 12:07:13 +0100

    Add dunder string method to Song model
    To be able to have more information about the
    song in the Ratings table in the admin view.

--- song_rater/models/song.py
+++ song_rater/models/song.py
@@ -6,6 +6,9 @@ class Song(models.Model):
     title = models.CharField(max_length=100)
     artist = models.CharField(max_length=100)
 
+    def __str__(self):
+        return f'{self.title} (by {self.artist})'
+
     class Meta:
         constraints = [
             models.UniqueConstraint(fields=['title', 'artist'], name='unique_song')
